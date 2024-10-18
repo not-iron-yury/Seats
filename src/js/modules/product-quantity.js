b9b1c8928dbb713fc7320cee9ee3cf41c22a8c9d
Yury Notiron <not.iron.yuriy@inbox.ru>
2024-10-18 16:13:55 +0300

    Мелкие доработки  + Changes to be committed: 	modified:   src/html/blocks/_header.html 	modified:   src/js/modules/product-quantity.js 	modified:   src/scss/blocks/products.scss
    	new file:   src/html/blocks/cart-section.html
    	new file:   "src/html/\321\201art.html"

--- src/js/modules/product-quantity.js
+++ src/js/modules/product-quantity.js
@@ -3,16 +3,18 @@ function productQuantity() {
   const buttonIncrease = document.getElementById('button-increase');
   const quantityInput = document.getElementById('quantity-input');
 
-  buttonDecrease.onclick = decreaseQuantity;
-  buttonIncrease.onclick = increaseQuantity;
+  if (quantityInput) {
+    buttonDecrease.onclick = decreaseQuantity;
+    buttonIncrease.onclick = increaseQuantity;
 
-  function decreaseQuantity() {
-    if (quantityInput.value > 1) {
-      quantityInput.value = parseInt(quantityInput.value) - 1;
+    function decreaseQuantity() {
+      if (quantityInput.value > 1) {
+        quantityInput.value = parseInt(quantityInput.value) - 1;
+      }
     }
-  }
-  function increaseQuantity() {
-    quantityInput.value = parseInt(quantityInput.value) + 1;
+    function increaseQuantity() {
+      quantityInput.value = parseInt(quantityInput.value) + 1;
+    }
   }
 }
 
